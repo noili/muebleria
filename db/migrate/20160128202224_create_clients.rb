5fbc61b0b51a84344cbc3c43aa643573355e79f5
noili <federico.neuman@gmail.com>
2016-01-28 17:27:51 -0300

    Created model for client.

---
+++ db/migrate/20160128202224_create_clients.rb
@@ -0,0 +1,14 @@
+class CreateClients < ActiveRecord::Migration
+  def change
+    create_table :clients do |t|
+      t.integer :dni
+      t.string :name
+      t.string :adress
+      t.string :email
+      t.string :phone
+      t.string :cellphone
+
+      t.timestamps null: false
+    end
+  end
+end
