754d08fa75fd3de544a02eab833c7b55d80050e2
noili <federico.neuman@gmail.com>
2016-01-29 20:04:06 -0300

    Refactor: client_controller.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,24 +1,26 @@
 class ClientsController < ApplicationController
 
   def create
-    @client = Client.new client_params
-    @client.save
-    redirect_to edit_client_path(@client.dni)
+    client = Client.create client_params
+    redirect_to edit_client_path(client.dni)
   end
 
   def edit
-    @client = Client.find_or_initialize_by dni: params[:dni]
+    client
   end
 
   def update
-    @client = Client.find_or_initialize_by dni: params[:dni]
-    @client.update client_params
-    redirect_to edit_client_path(@client.dni)
+    client.update client_params
+    redirect_to edit_client_path(client.dni)
   end
 
   private
-    def client_params
-      params.require(:client).permit(:dni, :name, :email, :adress, :phone, :cellphone)
-    end
 
+  def client_params
+      params.require(:client).permit(:dni, :name, :email, :adress, :phone, :cellphone)
+  end
+
+  def client
+    @client ||= Client.find_or_initialize_by dni: params[:dni]
+  end
 end
---
+++ test/controllers/clients_controller_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class ClientsControllerTest < ActionController::TestCase
+  test "existing client" do
+    john = clients :john
+    put :update, dni: john.dni, client: {name: 'pepe'}
+    updated_john = Client.find(john.id)
+    assert updated_john.name == 'pepe'
+  end
+
+  test "unexisting client" do
+    put :update, dni: '55555555', client: {name: 'jane'}
+    last_client = Client.last
+    assert last_client.name == 'jane'
+    assert last_client.dni == '55555555'
+  end
+end
