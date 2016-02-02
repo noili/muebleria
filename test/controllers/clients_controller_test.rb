fac9a85ac100208408be93670741fbcd6ae8b6a8
noili <federico.neuman@gmail.com>
2016-02-02 13:38:59 -0300

    Edit test.

--- test/controllers/clients_controller_test.rb
+++ test/controllers/clients_controller_test.rb
@@ -1,15 +1,20 @@
 require 'test_helper'
 
 class ClientsControllerTest < ActionController::TestCase
-  test "update existing client" do
+  test 'update existing client' do
     john = clients :john
     put :update, dni: john.dni, client: {name: 'pepe'}
     assert john.reload.name == 'pepe'
   end
 
-  test "should create unexisting client" do
+  test 'should create unexisting client' do
     assert_difference 'Client.count' do
       put :update, dni: '35353535', client: {name: 'ceci'}
     end
+  end
+
+  test 'client edit form' do
+    get :edit, dni: '33333333'
+    assert_response :success
   end
 end
