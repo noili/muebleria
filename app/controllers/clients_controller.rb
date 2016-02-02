bbaf78a3ea0a9e44e6dcef9ef33ccbc2d8a8f398
noili <federico.neuman@gmail.com>
2016-02-02 12:28:30 -0300

    Refactor.
    Changed text_field for email_field in view.
    Used to_param to remove repetition.
    Refactor ClientsControllerTest.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,7 +2,7 @@ class ClientsController < ApplicationController
 
   def create
     client = Client.create client_params
-    redirect_to edit_client_path(client.dni)
+    redirect_to edit_client_path
   end
 
   def edit
@@ -11,10 +11,14 @@ class ClientsController < ApplicationController
 
   def update
     client.update client_params
-    redirect_to edit_client_path(client.dni)
+    redirect_to edit_client_path
   end
 
   private
+
+  def to_param
+    dni
+  end
 
   def client_params
     params.require(:client).permit(:dni, :name, :email, :adress, :phone, :cellphone)
--- test/controllers/clients_controller_test.rb
+++ test/controllers/clients_controller_test.rb
@@ -1,18 +1,10 @@
 require 'test_helper'
 
 class ClientsControllerTest < ActionController::TestCase
-  test "existing client" do
+  test "update existing client" do
     john = clients :john
     put :update, dni: john.dni, client: {name: 'pepe'}
-    updated_john = Client.find(john.id)
-    assert updated_john.name == 'pepe'
-  end
-
-  test "unexisting client" do
-    put :update, dni: '55555555', client: {name: 'jane'}
-    last_client = Client.last
-    assert last_client.name == 'jane'
-    assert last_client.dni == '55555555'
+    assert john.reload.name == 'pepe'
   end
 
   test "should create unexisting client" do
