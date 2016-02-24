340d7e647f5a6027b481186c46cacea25b783ced
olvap <oldani.pablo@gmail.com>
2016-02-24 17:01:56 -0300

    Fix: calendar view.

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -5,7 +5,7 @@ $(document)
     $('fieldset#new-turn').hide();
     $('fieldset#add-to-turn').show();
   })
-  .on('click', '.calendar button.new-turn[data-at]', function (evt) {
+  .on('click', '.calendar h1 button[data-at]', function (evt) {
     $('#visit_turn_attributes_at').val($(this).data('at'));
     $('#visit_turn_attributes_employee_id').focus();
     $('#visit_turn_id').val('');
