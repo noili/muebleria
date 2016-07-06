6d7c00a507dd53fd5ab4adf555954a4d8dd7ebb4
Eloy Espinaco <eloyesp@gmail.com>
2016-07-06 11:55:15 -0300

    Add more integration tests

--- test/integration/admin_flow_test.rb
+++ test/integration/admin_flow_test.rb
@@ -8,8 +8,7 @@ class AdminFlowTest < ActionDispatch::IntegrationTest
     client_cellphone: '155-325565'
   }
 
-  test 'adding a new visit for a new client' do
-    dni = '302020202'
+  def login user: users(:admin)
     visit '/'
     page
     assert_current_path new_session_path
@@ -17,6 +16,10 @@ class AdminFlowTest < ActionDispatch::IntegrationTest
     fill_in :password, with: 'secret'
     click_on 'Iniciar'
     assert_current_path new_client_path
+  end
+
+  def create_client dni: '302020202'
+    visit new_client_path
     fill_in 'Buscar DNI', with: dni
     click_on 'Search'
     assert_current_path clients_path
@@ -26,10 +29,28 @@ class AdminFlowTest < ActionDispatch::IntegrationTest
     select 'sur', from: 'client_zone_id'
     click_on 'Guardar'
     assert_current_path new_client_visit_path(dni)
+  end
+
+  test 'adding a new visit on an existing turn' do
+    login
+    create_client
     fill_in :visit_description, with: 'una cama'
     fill_in :visit_duration, with: '2'
-    select Turn.last.to_s, from: 'Turno'
+    select turns(:tomorrow).to_s, from: 'Turno'
     click_on 'Guardar'
-    assert_current_path visit_path(Visit.last)
+    assert page.has_content? 'Visita creada'
+  end
+
+  test 'adding a new visit on a new turn' do
+    login
+    create_client
+    fill_in :visit_description, with: 'una cama'
+    fill_in :visit_duration, with: '2'
+    within("fieldset#new-turn") do
+      fill_in 'Fecha', :with => (Date.today + 3)
+      select 'Pablo', :from => 'Employee'
+    end
+    click_on 'Guardar'
+    assert page.has_content? 'Visita creada'
   end
 end
