c689ac4ce9c283125afb6c0733f8f63a5243545e
noili <federico.neuman@gmail.com>
2016-09-12 17:21:20 -0300

    Add errors to client/edit.html.erb.
    close #47

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,8 +2,13 @@ class Client < ActiveRecord::Base
   has_many :visits
   belongs_to :zone, required: true
   validates :address, presence: true
-  validates_presence_of :phone, unless: :cellphone?
-  validates_presence_of :cellphone, unless: :phone?
+  validate :phone_or_cellphone
+
+  def phone_or_cellphone
+    if phone.empty? && cellphone.empty?
+      errors.add(:phones, "either phone or cellphone can't be blank")
+    end
+  end
 
   def to_param
     dni
